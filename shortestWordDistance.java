c2509dce14d2535fac2640022a5d5dfc244e4efe
Weijing Tang <wtang35@wisc.edu>
2018-09-22 19:02:01 -0500

    Question244
    Question244

---
+++ shortestWordDistance.java
@@ -0,0 +1,18 @@
+package question244;
+
+public class shortestWordDistance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+    public static WordDistance(String[] words) {
+        
+    }
+    
+    public static int shortest(String word1, String word2) {
+        
+    }
+
+}
