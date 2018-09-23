b7da6d333f46e6d2923be1f6a14568e933a8095d
Weijing Tang <wtang35@wisc.edu>
2018-09-22 19:42:37 -0500

    Question10
    Question10

---
+++ regularExpressionMatching.java
@@ -0,0 +1,20 @@
+package question10;
+
+public class regularExpressionMatching {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      String s="aa";
+      String p=".*";
+      System.out.println(isMatch(a,p));		
+		
+	}
+	
+    public static boolean isMatch(String s, String p) {
+    	
+    	  
+    	
+        
+    }
+
+}
